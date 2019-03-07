a0aee84eb27c5a759bc6cc29f8499d3691b32f3d
Steve Kinney <hello@stevekinney.net>
2019-03-07 10:13:12 +0000

    Get the "Open File" button working

--- app/main.js
+++ app/main.js
@@ -1,1 +1,40 @@
-console.log('This is an Electron application!');
+const fs = require('fs');
+const { app, BrowserWindow, dialog } = require('electron');
+
+let mainWindow;
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({ show: false });
+
+  mainWindow.loadFile(`${__dirname}/index.html`);
+
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show();
+  });
+
+  mainWindow.on('close', () => {
+    mainWindow = null;
+  });
+});
+
+const getFileFromUser = exports.getFileFromUser = () => {
+  const files = dialog.showOpenDialog(mainWindow, {
+    properties: ['openFile'],
+    filters: [
+      { name: 'Markdown Files', extensions: ['md', 'markdown', 'mdown']},
+      { name: 'Text Files', extensions: ['txt', 'text']},
+    ],
+    title: 'Open a Very Cool File',
+    buttonLabel: 'Open 🇬🇧'
+  });
+
+  if (!files) return;
+
+  const file = files[0];
+  openFile(file);
+};
+
+const openFile = (file) => {
+  const content = fs.readFileSync(file).toString();
+  mainWindow.webContents.send('file-opened', file, content);
+};
